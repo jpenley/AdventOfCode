4e0efc89b7e6ff57f47bb95b909d4bd088212542
Jason Penley <darkwyrd@hotmail.com>
2025-12-03 22:03:06 -0700

    Starting

---
+++ AoC2025/Day04.cs
@@ -0,0 +1,16 @@
+﻿using Shared;
+
+namespace AoC2025;
+
+public class Day04(ILineLoader loader) : IDay
+{
+    private ILineLoader loader = loader;
+
+    public string Year => "2025";
+    public string Name => "Day ";
+    
+    public string Part1()
+    {
+        
+    }
+}
